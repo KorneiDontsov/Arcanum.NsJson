25f13a3133a891144007e01b3bfbd7eec7964962
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-24 21:13:45 +0300

    Implement 'ArcanumJsonContractResolver.shared'
    To reduce memory usage.

--- Source/Arcanum.ForNewtonsoftJson/api/foundation/implementations/ArcanumJsonContractResolver.cs
+++ Source/Arcanum.ForNewtonsoftJson/api/foundation/implementations/ArcanumJsonContractResolver.cs
@@ -1,8 +1,26 @@
 ﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
 
+using System;
+
 using Newtonsoft.Json.Serialization;
 
 namespace Arcanum.ForNewtonsoftJson
 {
-	public class ArcanumJsonContractResolver : DefaultContractResolver { }
+	public class ArcanumJsonContractResolver : DefaultContractResolver
+	{
+		private sealed class Immutable : IContractResolver
+		{
+			private ArcanumJsonContractResolver _core { get; }
+
+			public Immutable ()
+			{
+				_core = new ArcanumJsonContractResolver();
+			}
+
+			/// <inheritdoc />
+			public JsonContract ResolveContract (Type type) => _core.ResolveContract(type);
+		}
+
+		public static IContractResolver shared { get; } = new Immutable();
+	}
 }
