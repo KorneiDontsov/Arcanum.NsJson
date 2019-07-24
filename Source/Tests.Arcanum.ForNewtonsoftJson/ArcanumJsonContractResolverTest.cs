ef5a5cf65405bfd54c47e5bfd0daf84d863e0bdb
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-24 22:05:54 +0300

    Test property-backing field serialization

---
+++ Source/Tests.Arcanum.ForNewtonsoftJson/ArcanumJsonContractResolverTest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using Arcanum.ForNewtonsoftJson;
+
+using Newtonsoft.Json;
+
+namespace Tests.Arcanum.ForNewtonsoftJson
+{
+	public abstract class ArcanumJsonContractResolverTest
+	{
+		protected JsonSerializer serializer { get; }
+
+		protected ArcanumJsonContractResolverTest ()
+		{
+			serializer = new JsonSerializer { ContractResolver = new ArcanumJsonContractResolver() };
+		}
+	}
+}
