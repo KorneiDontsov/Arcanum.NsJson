a2fc9a1a1813cb2fe5550f918945f9e76c9de5a4
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-24 20:39:09 +0300

    Add 'ArcanumJsonContractResolver' as mainline of v0.1

---
+++ Source/Arcanum.ForNewtonsoftJson/api/foundation/implementations/ArcanumJsonContractResolver.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using Newtonsoft.Json.Serialization;
+
+namespace Arcanum.ForNewtonsoftJson
+{
+	public class ArcanumJsonContractResolver : DefaultContractResolver { }
+}
