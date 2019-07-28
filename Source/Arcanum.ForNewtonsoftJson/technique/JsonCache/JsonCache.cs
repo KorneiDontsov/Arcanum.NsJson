392e59793eb3392ca4fc7890c17bce2e5fb0e8c2
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-28 15:46:40 +0300

    Add 'JsonCache' with factory
    Help to stash the intermediate values while converting from/to json.

---
+++ Source/Arcanum.ForNewtonsoftJson/technique/JsonCache/JsonCache.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Arcanum.ForNewtonsoftJson
+{
+	public readonly struct JsonCache : IDisposable
+	{
+		private MemoryStream _implStream { get; }
+
+		internal JsonCache (MemoryStream implStream)
+		{
+			_implStream = implStream;
+		}
+
+		/// <inheritdoc />
+		public void Dispose () => _implStream.Dispose();
+
+		public JsonWriter OpenToWrite ()
+		{
+			_implStream.Position = 0;
+
+			return new JsonTextWriter(new StreamWriter(_implStream, Encoding.UTF8, bufferSize: 1024, leaveOpen: true));
+		}
+
+		public JsonReader OpenToRead ()
+		{
+			_implStream.Position = 0;
+
+			return new JsonTextReader(
+				new StreamReader(
+					_implStream,
+					Encoding.UTF8,
+					detectEncodingFromByteOrderMarks: false,
+					bufferSize: 1024,
+					leaveOpen: true
+				)
+			);
+		}
+	}
+}
