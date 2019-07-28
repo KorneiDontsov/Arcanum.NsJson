ba0ab47c9c5db528d1cb0c156e04291f7fedc59d
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-07-29 00:54:12 +0300

    Implement discriminated union deserialization
    Refactor 'DiscriminatedUnionJsonReadConverter'

---
+++ Source/Arcanum.ForNewtonsoftJson/api/foundation/exceptions/JsonContractResolveException.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System;
+
+using Newtonsoft.Json;
+
+namespace Arcanum.ForNewtonsoftJson
+{
+	public sealed class JsonContractResolveException : JsonException
+	{
+		public JsonContractResolveException (String message, Exception? innerException = null)
+		: base(message, innerException) { }
+	}
+}
--- Source/Arcanum.ForNewtonsoftJson/api/foundation/implementations/ArcanumJsonContractResolver.cs
+++ Source/Arcanum.ForNewtonsoftJson/api/foundation/implementations/ArcanumJsonContractResolver.cs
@@ -4,12 +4,62 @@ using System;
 using System.Collections.Concurrent;
 using System.Threading;
 
+using Arcanum.DataContracts;
+
 using Newtonsoft.Json.Serialization;
 
 namespace Arcanum.ForNewtonsoftJson
 {
 	public class ArcanumJsonContractResolver : DefaultContractResolver
 	{
+		public sealed class Dependencies
+		{
+			public IDataTypeInfoStorage dataTypeInfoStorage { get; }
+
+			public static Dependencies @default { get; } = new Dependencies(DataTypeInfoStorage.shared);
+
+			public Dependencies (IDataTypeInfoStorage dataTypeInfoStorage)
+			{
+				this.dataTypeInfoStorage = dataTypeInfoStorage;
+			}
+		}
+
+		protected Dependencies dependencies { get; }
+
+		private MiddlewareContractStorage _middlewareContractStorage { get; }
+
+		public ArcanumJsonContractResolver (Dependencies dependencies)
+		{
+			this.dependencies = dependencies;
+			_middlewareContractStorage = new MiddlewareContractStorage(this);
+
+			configure();
+		}
+
+		public ArcanumJsonContractResolver () : this(Dependencies.@default) { }
+
+		private void configure ()
+		{
+			SerializeCompilerGeneratedMembers = true;
+		}
+
+		#region resolve
+		private sealed class MiddlewareContractStorage
+		{
+			private ConcurrentDictionary<Type, JsonContract> _dict { get; }
+
+			private Func<Type, JsonContract> _contractCreator { get; }
+
+			public MiddlewareContractStorage (ArcanumJsonContractResolver resolver)
+			{
+				_dict = new ConcurrentDictionary<Type, JsonContract>();
+				_contractCreator = resolver.ConstructMiddlewareContract;
+			}
+
+			public JsonContract Get (Type type) => _dict.GetOrAdd(type, _contractCreator);
+		}
+
+		/// <inheritdoc />
 		internal readonly struct ResolveContractArgs
 		{
 			internal readonly struct Scope : IDisposable
@@ -41,52 +91,78 @@ namespace Arcanum.ForNewtonsoftJson
 			}
 		}
 
-		private sealed class MiddlewareContractStorage
-		{
-			private ConcurrentDictionary<Type, JsonContract> _dict { get; }
-
-			private Func<Type, JsonContract> _contractCreator { get; }
-
-			public MiddlewareContractStorage (ArcanumJsonContractResolver resolver)
-			{
-				_dict = new ConcurrentDictionary<Type, JsonContract>();
-				_contractCreator = resolver.CreateMiddlewareContract;
-			}
-
-			public JsonContract Get (Type type) => _dict.GetOrAdd(type, _contractCreator);
-		}
-
-		private MiddlewareContractStorage _middlewareContractStorage { get; }
-
-		public ArcanumJsonContractResolver ()
-		{
-			_middlewareContractStorage = new MiddlewareContractStorage(this);
-
-			configure();
-		}
-
-		private void configure ()
-		{
-			SerializeCompilerGeneratedMembers = true;
-		}
-
-		/// <inheritdoc />
 		public sealed override JsonContract ResolveContract (Type type)
 		{
 			var args = ResolveContractArgs.Pick();
 
 			return args.withoutMiddleware
-			? base.ResolveContract(type)
-			: _middlewareContractStorage.Get(type);
+			? ResolveContractWithoutMiddleware(type)
+			: ResolveContractWithMiddleware(type);
 		}
 
-		private JsonContract CreateMiddlewareContract (Type objectType)
+		protected JsonContract ResolveContractWithoutMiddleware (Type type)
 		{
-			var baseContract = base.ResolveContract(objectType);
-			return ApplyMiddleware(baseContract);
+			return base.ResolveContract(type);
 		}
 
-		protected virtual JsonContract ApplyMiddleware (JsonContract contract) => contract;
+		protected JsonContract ResolveContractWithMiddleware (Type type)
+		{
+			return _middlewareContractStorage.Get(type);
+		}
+
+		private JsonContract ConstructMiddlewareContract (Type objectType)
+		{
+			var baseContract = ResolveContractWithoutMiddleware(objectType);
+			return CreateMiddlewareContract(baseContract);
+		}
+
+		protected virtual JsonContract CreateMiddlewareContract (JsonContract contract)
+		{
+			if (contract.UnderlyingType.IsClass)
+			{
+				if (contract.Converter is null && contract.UnderlyingType.IsAbstract)
+				{
+					var maybeDiscriminatedUnionInfo = dependencies.dataTypeInfoStorage.Get(contract.UnderlyingType)
+					.asDiscriminatedUnionInfo;
+
+					if (maybeDiscriminatedUnionInfo != null)
+					{
+						return CreateDiscriminatedUnionMiddlewareContract(contract, maybeDiscriminatedUnionInfo);
+					}
+				}
+			}
+
+			return contract;
+		}
+
+		/// <param name="discriminatedUnionInfo">
+		///     Must be of type <see cref="JsonContract.UnderlyingType" /> specified in
+		///     <paramref name="contract" />.
+		/// </param>
+		protected virtual JsonContract CreateDiscriminatedUnionMiddlewareContract (
+			JsonContract contract,
+			DiscriminatedUnionInfo discriminatedUnionInfo
+		)
+		{
+			if (contract.UnderlyingType != discriminatedUnionInfo.dataTypeInfo.dataType)
+			{
+				throw new Exception(
+					$"'{nameof(discriminatedUnionInfo)}' {discriminatedUnionInfo} doesn't correspond to 'contract' of type {contract.UnderlyingType}."
+				);
+			}
+			else if (discriminatedUnionInfo.hasErrors)
+			{
+				throw new JsonContractResolveException(
+					$"Cannot resolve {discriminatedUnionInfo.dataType} because it has errors.\n{discriminatedUnionInfo.GetErrorString()}"
+				);
+			}
+
+			var middlewareContract = contract.Copy();
+			middlewareContract.Converter = new DiscriminatedUnionJsonReadConverter(discriminatedUnionInfo);
+
+			return middlewareContract;
+		}
+		#endregion
 
 		#region shared
 		private sealed class Immutable : IContractResolver
