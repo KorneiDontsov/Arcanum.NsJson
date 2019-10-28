ab1e2be0b3ff872fa084e66486ee8619223863f9
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-10-28 17:28:49 +0300

    Redesign main project

---
+++ Source/Arcanum.NsJson/api/foundation/JsonContractResolverFactory.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+namespace Arcanum.NsJson {
+	using Arcanum.NsJson.ContractModules;
+	using Arcanum.NsJson.ContractResolvers;
+	using Newtonsoft.Json.Serialization;
+	using System.Net;
+
+	static class JsonContractResolverFactory {
+		public static IContractResolver @default { get; }
+
+		static JsonContractResolverFactory () {
+			var contractResolverCore = new JsonContractResolverCore();
+			var jsonContractFactory =
+				JsonContractFactory.Build()
+					.With<UnionJsonContractModule>()
+					.With<EnumJsonContractModule>()
+					.With<IPAddress, IpAddressJsonContractModule>()
+					.With<DnsEndPoint, DnsEndPointJsonContractModule>()
+					.Ok();
+			var middlewareJsonContractFactory =
+				MiddlewareJsonContractFactory.Build()
+					.With<UnionCaseJsonMiddlewareModule>()
+					.Ok();
+			@default =
+				new JsonContractResolver(contractResolverCore, jsonContractFactory, middlewareJsonContractFactory);
+		}
+	}
+}
